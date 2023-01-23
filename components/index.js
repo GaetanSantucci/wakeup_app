9cfd2967fbc70ae41594ac2d5f015a7a044b18f2
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2023-01-23 23:16:12 +0100

    :boom: refacto component Products

--- components/index.js
+++ components/index.js
@@ -1,7 +1,8 @@
 //& Imports Components
 import Header from './Header';
 import Articles from './Articles';
+import Products from './Products';
 // import Spinner from './Spinner/Spinner';
 import Footer from './Footer';
 
-export { Header, Articles, Footer };
+export { Header, Articles, Products, Footer };
