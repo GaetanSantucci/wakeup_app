93f4af0ce8ed401c19aa7c62bb0b760fc2eaf589
GaetanSantucci <gaetan.santucci@outlook.com>
2023-02-06 13:09:07 +0100

    :bug: fix any deploy bug

--- app/business/page.jsx
+++ app/business/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-import styles from '../page.module.scss';
 import { Business, ScrollUp } from '@/components';
 
 import { usePathname } from 'next/navigation';
--- app/producers/page.jsx
+++ app/producers/page.jsx
@@ -1,4 +1,4 @@
-import styles from '../page.module.scss';
+import styles from '/public/Homepage.module.scss';
 
 export default function producer() {
   return (
