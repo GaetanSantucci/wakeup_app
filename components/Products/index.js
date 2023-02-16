28129670c3a8e3cdf181c0104ed9ccfd80c01c61
GaetanSantucci <gaetan.santucci@outlook.com>
2023-02-16 16:18:43 +0100

    :rocket: responsive page contact

--- components/Products/index.js
+++ components/Products/index.js
@@ -11,7 +11,7 @@ export default async function Products() {
 
   return (
     <>
-      <h2 className={styles.product__title}>DES PLATEAUX GOURMANDS ET ÉLÉGANTS</h2>
+      <h2 style={{ textAlign: 'center' }}>DES PLATEAUX GOURMANDS ET ÉLÉGANTS</h2>
       <p className={styles.product__text}>Nous vous proposons des plateaux gourmands, élégants et raffinés. Du petit-déjeuner, au brunch et même au plateau apéritif nous proposons une selection de plateaux avec des produits de qualité.</p>
       <p className={styles.product__text}>Nous avons à cœur de travailler avec des artisans locaux et nous concoctons également la plupart de nos produits car ici nous prônons le fait-maison !
       </p>
